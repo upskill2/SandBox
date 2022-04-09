e40a2752a89ce6e3e03010457f207993f0f15c0c
TChmeruk <6105_7@ukr.net>
2022-04-09 13:11:10 +0300

    update

---
+++ src/main/java/leetcode/ReverseVowelsOfAString.java
@@ -0,0 +1,7 @@
+package leetcode.tests;
+
+public class ReverseVowelsOfAString {
+
+    
+
+}
---
+++ src/main/java/leetcode/tests/ReverseStringTest.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+
+    @BeforeEach
+    void setUp() {
+char [] chars = {'a','m','a','n','a','P',' ',':','l','a','n','a','c',' ',' ','a',',','n','a','l','p',' ','a',' ',',','n','a','m',' ','A'};
+
+    }
+
+    @Test
+    void reverseString() {
+    }
+
+}
