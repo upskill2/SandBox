492f5b7ff24bccf7d3f991f5adc63df7d2d25b9c
TChmeruk <6105_7@ukr.net>
2022-05-24 00:48:39 +0300

    may 24

--- src/main/java/leetcode/LastStoneWeight.java
+++ src/main/java/leetcode/LastStoneWeight.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,20 +8,49 @@ public class LastStoneWeight {
 
     public int lastStoneWeight(int[] stones) {
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> arr = Arrays
+                .stream(stones)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
 
-    //    List<Integer> l1 = Stream..of(stones).collect(Collectors.toCollection(ArrayList::new));
 
 
-        return 1;
+        int res = getStones(arr);
+
+        return res;
+    }
+
+    private int getStones(List<Integer> lst){
+
+        while(lst.size()>0){
+            if(lst.size()==1){
+                return lst.get(0);
+            }
+
+            else if(lst.get(0)== lst.get(1)){
+                lst.remove(0);
+                lst.remove(0);
+            } else if(lst.get(0)!= lst.get(1)){
+                lst.set(0,lst.get(0)-lst.get(1));
+                lst.remove(1);
+            }
+            lst.sort(Comparator.reverseOrder());
+            getStones(lst);
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
 
-        int[] stones = {2,7,4,1,8,1};
+        int[] stones = {2, 7, 4, 1, 8, 1};
+        int[] stones1 = {1};
+        int[] stones2 = {2,2};
 
         LastStoneWeight last = new LastStoneWeight();
-        last.lastStoneWeight(stones);
+        // last.lastStoneWeight(stones);
+        // last.lastStoneWeight(stones1);
+        last.lastStoneWeight(stones2);
 
     }
 
